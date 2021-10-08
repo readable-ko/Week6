49ce7cec42efba6f1f6b3b63ca14af85ee03da72
readable-ko <21700026@handong.edu>
2021-10-08 10:48:37 +0900

    updated txt to db

--- src/com/todo/dao/TodoList.java
+++ src/com/todo/dao/TodoList.java
@@ -1,16 +1,24 @@
 package com.todo.dao;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.*;
 
+import com.todo.service.DbConnect;
 import com.todo.service.TodoSortByDate;
 import com.todo.service.TodoSortByDateDesc;
 import com.todo.service.TodoSortByName;
 
 public class TodoList {
+	Connection conn;
 	private List<TodoItem> list;
 
 	public TodoList() {
-		this.list = new ArrayList<TodoItem>();
+		//this.list = new ArrayList<TodoItem>();
+		this.conn = DbConnect.getConnection();
 	}
 
 	public void addItem(TodoItem t) {
@@ -88,4 +96,40 @@ public class TodoList {
 		}
 		return false;
 	}
+	
+	public void importData(String filename) {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader (filename));
+			String line;
+			String sql = "insert into list (title, memo, category, current_data, due_date)"
+					+ "values (?,?,?,?,?);";
+			int records = 0;
+			while((line = br.readLine()) != null) {
+				StringTokenizer st = new StringTokenizer(line,"##");
+				
+				String category = st.nextToken();
+				String title = st.nextToken();
+				String description = st.nextToken();
+				String due_date = st.nextToken();
+				String current_date = st.nextToken();
+				
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, title);
+				pstmt.setString(2, description);
+				pstmt.setString(3, category);
+				pstmt.setString(4, current_date);
+				pstmt.setString(5, due_date);
+				
+				int count = pstmt.executeUpdate();
+				if(count > 0) records++;
+				pstmt.close();
+			}
+			System.out.println(records + " records read!!");
+			br.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
